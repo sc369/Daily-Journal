588b549f5415a07dbec95b139ac2d085ca445d00
Stephen Clark <stephenc369@gmail.com>
2019-01-22 09:23:11 -0600

    added json

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,17 +1,34 @@
 const allJournalEntries = []
 
-function makeJournalEntry(thisDate, thisTitle, thisTextEntry, thisMood) {
+function makeJournalEntry(thisDate, thisTitle, thisTextEntry, thisMood, thisId) {
     let journalEntry = {
         date: thisDate,
         title: thisTitle,
         text: thisTextEntry,
         mood: thisMood,
+        id: thisId
     }
     allJournalEntries.push(journalEntry)
 }
 
-makeJournalEntry("Jan 14", "ChickenMonkey", "Chicken Monkey was interesting", "happy")
-makeJournalEntry("Jan 14", "Concatenating output", "This was an elegant way to do the exercise", "happy")
+makeJournalEntry("Jan 14", "ChickenMonkey", "Chicken Monkey was interesting", "happy", 1)
+makeJournalEntry("Jan 14", "Concatenating output", "This was an elegant way to do the exercise", "happy", 2)
 
 console.log(allJournalEntries[0])
 console.log(allJournalEntries[1])
+
+const createJournalHTML = (entry) => `
+         <h3>${entry.title}</h3>
+         <p>${entry.date}</p>
+         <p>${entry.text}</p>
+         <p>mood: ${entry.mood}</p>
+        `
+
+const addToDom = (entryAsHtml) => {
+document.querySelector(".container").innerHTML += entryAsHtml
+}
+
+allJournalEntries.forEach(entry => {
+    const entryAsHtml = createJournalHTML(entry)
+    addToDom(entryAsHtml)
+})
