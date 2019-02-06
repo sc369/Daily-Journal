2674dc7af6e90352d037ee9c8b3cfc50d348dead
Stephen Clark <stephenc369@gmail.com>
2019-02-06 13:19:32 -0600

    updated fetch

---
+++ src/scripts/newjournal.js
@@ -0,0 +1,46 @@
+const createJournalHTML = (entry) => { `
+<article class = "journalEntry">         
+  <section class = "title">
+  <h3>${entry.title}</h3>
+  </section>
+  <section class = "date">
+  <p>${entry.date}</p>
+  </section>
+  <section class = "text">
+  <p>${entry.text}</p>
+  </section>
+  <section class = "mood">
+  <p>mood: ${entry.mood}</p>
+  </section>
+  `
+}
+
+
+
+const addToDom = (entryAsHtml) => {
+    document.querySelector(".container").innerHTML += entryAsHtml
+}
+
+const getEntries = () => {
+    return fetch("http://127.0.0.1:8088/entries")
+        .then(res => res.json())
+}
+
+
+
+
+const postEntry = (entryObject) => {
+    return fetch("http://127.0.0.1:8088/entries", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(orderObject)
+    })
+        .then(res => res.json())
+}
+const container = document.querySelector("#container")
+
+
+
+ 
--- src/scripts/oldjournal.js
+++ src/scripts/oldjournal.js
@@ -15,26 +15,36 @@ makeJournalEntry("Jan 14", "ChickenMonkey", "Chicken Monkey was interesting", "h
 makeJournalEntry("Jan 14", "Concatenating output", "This was an elegant way to do the exercise", "happy", 2)
 
 const createJournalHTML = (entry) => `
-         <h3>${entry.title}</h3>
-         <p>${entry.date}</p>
-         <p>${entry.text}</p>
-         <p>mood: ${entry.mood}</p>
-        `
+<article class = journalEntry>         
+  <section class = "title">
+  <h3>${entry.title}</h3>
+  </section>
+  <section class = "date">
+  <p>${entry.date}</p>
+  </section>
+  <section class = "text">
+  <p>${entry.text}</p>
+  </section>
+  <section class = "mood">
+  <p>mood: ${entry.mood}</p>
+  </section>
+</article>
+`
 
 const addToDom = (entryAsHtml) => {
-document.querySelector(".container").innerHTML += entryAsHtml
+    document.querySelector(".container").innerHTML += entryAsHtml
 }
 
 fetch('http://localhost:3000/entries')
-  .then(entries => entries.json())
-  .then(parsedEntries => {
-    parsedEntries.forEach(entry => {
-    const entryAsHtml = createJournalHTML(entry)
-    addToDom(entryAsHtml)
-})
-}) 
+    .then(entries => entries.json())
+    .then(parsedEntries => {
+        parsedEntries.forEach(entry => {
+            const entryAsHtml = createJournalHTML(entry)
+            addToDom(entryAsHtml)
+        })
+    })
 
-  
+
 
 allJournalEntries.forEach(entry => {
     const entryAsHtml = createJournalHTML(entry)
